5e9a8bf352b78293847f7cc63aa479795fb5fd58
Paul van Buuren <paul@wbvb.nl>
2017-06-04 20:43:19 +0200

    Script om e.e.a. te verplaatsen naar een testURL
    En ook een script om een kopie te maken van de tegeltjes uit de backup

--- tegelizr-get-tegeltjes.sh
+++ tegelizr-get-tegeltjes.sh
@@ -8,8 +8,10 @@ rsync -r -a  --delete  '/shared-paul-files/Backups/webfaction/tegelizr/thumbs/' 
 ##echo 'Tenslotte de DB';
 ##rsync -r -a  --delete  '/shared-paul-files/Backups/webfaction/tegelizr/alle-tegeltjes/' '/shared-paul-files/Webs/git-repos/tegelizr-source/alle-tegeltjes/'
 
+rm -rf /shared-paul-files/Webs/git-repos/tegelizr-source/deleted_files/thumbs/*;
 
-rm -rf /shared-paul-files/Webs/git-repos/tegelizr-source/deleted_files/tegeltjes/*
-rm -rf /shared-paul-files/Webs/git-repos/tegelizr-source/deleted_files/thumbs/*
+rm -rf /shared-paul-files/Webs/git-repos/tegelizr-source/deleted_files/tegeltjes/*;
+
+
 
 echo 'Klaar!';
