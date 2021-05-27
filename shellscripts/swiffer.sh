619c4b5981ef80c620d95415ffdcb59968c3e888
Paul van Buuren <paul@wbvb.nl>
2021-05-27 17:53:48 +0200

    automagisch ophoepelscript toegevoegd

---
+++ shellscripts/swiffer.sh
@@ -0,0 +1,20 @@
+# sh '/home/paulvanb/webapps/tegelizr/shellscripts/swiffer.sh'
+
+echo "Swiffer hiero"
+
+find /home/paulvanb/webapps/tegelizr/ -type f -name "*van-harte-gefeliciteerd*" -exec rm -vf {} \;
+find /home/paulvanb/webapps/tegelizr/ -type f -name "*hartelijk-gefeliciteerd*" -exec rm -vf {} \;
+find /home/paulvanb/webapps/tegelizr/ -type f -name "*fijne-samenwerking*" -exec rm -vf {} \;
+find /home/paulvanb/webapps/tegelizr/ -type f -name "*gefeliciteerd-met-je-verjaardag*" -exec rm -vf {} \;
+find /home/paulvanb/webapps/tegelizr/ -type f -name "*woordvoerder-oeleh*" -exec rm -vf {} \;
+find /home/paulvanb/webapps/tegelizr/ -type f -name "*woordvoerder-ouleh*" -exec rm -vf {} \;
+find /home/paulvanb/webapps/tegelizr/ -type f -name "*gaat-met-pensioen*" -exec rm -vf {} \;
+find /home/paulvanb/webapps/tegelizr/ -type f -name "*proficiat*" -exec rm -vf {} \;
+find /home/paulvanb/webapps/tegelizr/ -type f -name "*lokwinske*" -exec rm -vf {} \;
+find /home/paulvanb/webapps/tegelizr/ -type f -name "*langkous*" -exec rm -vf {} \;
+find /home/paulvanb/webapps/tegelizr/ -type f -name "*linkse-ratten*" -exec rm -vf {} \;
+find /home/paulvanb/webapps/tegelizr/ -type f -name "*goud-olie-en-drugs*" -exec rm -vf {} \;
+find /home/paulvanb/webapps/tegelizr/ -type f -name "*landverrader*" -exec rm -vf {} \;
+find /home/paulvanb/webapps/tegelizr/ -type f -name "*appy-birthday*" -exec rm -vf {} \;
+
+echo "Opgeruimd staat netjes"
