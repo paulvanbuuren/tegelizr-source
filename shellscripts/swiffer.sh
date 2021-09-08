8f39fdcc6e8e682cf1f124847c2d603c1a7b81b9
Paul van Buuren <paul@wbvb.nl>
2021-09-08 17:08:30 +0200

    meer automagische ophoepelwoorden

--- shellscripts/swiffer.sh
+++ shellscripts/swiffer.sh
@@ -14,7 +14,7 @@ find /home/paulvanb/webapps/tegelizr/ -type f -name "*lokwinske*" -exec rm -vf {
 find /home/paulvanb/webapps/tegelizr/ -type f -name "*langkous*" -exec rm -vf {} \;
 find /home/paulvanb/webapps/tegelizr/ -type f -name "*youtube*" -exec rm -vf {} \;
 find /home/paulvanb/webapps/tegelizr/ -type f -name "*2083*" -exec rm -vf {} \;
-
+find /home/paulvanb/webapps/tegelizr/ -type f -name "*snicker*" -exec rm -vf {} \;
 find /home/paulvanb/webapps/tegelizr/ -type f -name "*linkse-ratten*" -exec rm -vf {} \;
 find /home/paulvanb/webapps/tegelizr/ -type f -name "*goud-olie-en-drugs*" -exec rm -vf {} \;
 find /home/paulvanb/webapps/tegelizr/ -type f -name "*landverrader*" -exec rm -vf {} \;
@@ -29,6 +29,7 @@ find /home/paulvanb/webapps/tegelizr/ -type f -name "*aivd-luistert-mee*" -exec 
 find /home/paulvanb/webapps/tegelizr/ -type f -name "*frambozenjam*" -exec rm -vf {} \;
 find /home/paulvanb/webapps/tegelizr/ -type f -name "*drek-drek*" -exec rm -vf {} \;
 find /home/paulvanb/webapps/tegelizr/ -type f -name "*lala-lala*" -exec rm -vf {} \;
+find /home/paulvanb/webapps/tegelizr/ -type f -name "*langkous*" -exec rm -vf {} \;
 rm /home/paulvanb/webapps/tegelizr/deleted_files/thumbs/*;
 rm /home/paulvanb/webapps/tegelizr/deleted_files/tegeltjes/*;
 
