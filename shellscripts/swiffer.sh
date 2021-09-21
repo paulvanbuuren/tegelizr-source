c9cd0e868437666d2dfa544597df4b106acf8d66
Paul van Buuren <paul@wbvb.nl>
2021-09-21 22:25:37 +0200

    swiffer bijgewerkt

--- shellscripts/swiffer.sh
+++ shellscripts/swiffer.sh
@@ -1,40 +1,39 @@
-# sh '/var/www/vhosts/plaatjesgenerator.nl/tegelizr/shellscripts/swiffer.sh'
+# sh '/var/www/vhosts/plaatjesgenerator.nl/tegelizr.nl/shellscripts/swiffer.sh'
 
 echo "Swiffer hiero"
 
-find /var/www/vhosts/plaatjesgenerator.nl/tegelizr/ -type f -name "*van-harte-gefeliciteerd*" -exec rm -vf {} \;
-find /var/www/vhosts/plaatjesgenerator.nl/tegelizr/ -type f -name "*hartelijk-gefeliciteerd*" -exec rm -vf {} \;
-find /var/www/vhosts/plaatjesgenerator.nl/tegelizr/ -type f -name "*fijne-samenwerking*" -exec rm -vf {} \;
-find /var/www/vhosts/plaatjesgenerator.nl/tegelizr/ -type f -name "*gefeliciteerd-met-je-verjaardag*" -exec rm -vf {} \;
-find /var/www/vhosts/plaatjesgenerator.nl/tegelizr/ -type f -name "*woordvoerder-oeleh*" -exec rm -vf {} \;
-find /var/www/vhosts/plaatjesgenerator.nl/tegelizr/ -type f -name "*woordvoerder-ouleh*" -exec rm -vf {} \;
-find /var/www/vhosts/plaatjesgenerator.nl/tegelizr/ -type f -name "*gaat-met-pensioen*" -exec rm -vf {} \;
-find /var/www/vhosts/plaatjesgenerator.nl/tegelizr/ -type f -name "*proficiat*" -exec rm -vf {} \;
-find /var/www/vhosts/plaatjesgenerator.nl/tegelizr/ -type f -name "*lokwinske*" -exec rm -vf {} \;
-find /var/www/vhosts/plaatjesgenerator.nl/tegelizr/ -type f -name "*langkous*" -exec rm -vf {} \;
-find /var/www/vhosts/plaatjesgenerator.nl/tegelizr/ -type f -name "*youtube*" -exec rm -vf {} \;
-find /var/www/vhosts/plaatjesgenerator.nl/tegelizr/ -type f -name "*2083*" -exec rm -vf {} \;
-find /var/www/vhosts/plaatjesgenerator.nl/tegelizr/ -type f -name "*snicker*" -exec rm -vf {} \;
-find /var/www/vhosts/plaatjesgenerator.nl/tegelizr/ -type f -name "*linkse-ratten*" -exec rm -vf {} \;
-find /var/www/vhosts/plaatjesgenerator.nl/tegelizr/ -type f -name "*goud-olie-en-drugs*" -exec rm -vf {} \;
-find /var/www/vhosts/plaatjesgenerator.nl/tegelizr/ -type f -name "*landverrader*" -exec rm -vf {} \;
-find /var/www/vhosts/plaatjesgenerator.nl/tegelizr/ -type f -name "*appy-birthday*" -exec rm -vf {} \;
-find /var/www/vhosts/plaatjesgenerator.nl/tegelizr/ -type f -name "*adolf-befbezem*" -exec rm -vf {} \;
-find /var/www/vhosts/plaatjesgenerator.nl/tegelizr/ -type f -name "*amoorah*" -exec rm -vf {} \;
-find /var/www/vhosts/plaatjesgenerator.nl/tegelizr/ -type f -name "*berber*" -exec rm -vf {} \;
-find /var/www/vhosts/plaatjesgenerator.nl/tegelizr/ -type f -name "*paul-van-buuren*" -exec rm -vf {} \;
-find /var/www/vhosts/plaatjesgenerator.nl/tegelizr/ -type f -name "*marokka*" -exec rm -vf {} \;
-find /var/www/vhosts/plaatjesgenerator.nl/tegelizr/ -type f -name "*lavendelnazi*" -exec rm -vf {} \;
-find /var/www/vhosts/plaatjesgenerator.nl/tegelizr/ -type f -name "*limburgers*" -exec rm -vf {} \;
-find /var/www/vhosts/plaatjesgenerator.nl/tegelizr/ -type f -name "*zuigen*" -exec rm -vf {} \;
-find /var/www/vhosts/plaatjesgenerator.nl/tegelizr/ -type f -name "*anaal-is-gewoon-astronomie*" -exec rm -vf {} \;
-find /var/www/vhosts/plaatjesgenerator.nl/tegelizr/ -type f -name "*aivd-luistert-mee*" -exec rm -vf {} \;
-find /var/www/vhosts/plaatjesgenerator.nl/tegelizr/ -type f -name "*frambozenjam*" -exec rm -vf {} \;
-find /var/www/vhosts/plaatjesgenerator.nl/tegelizr/ -type f -name "*drek-drek*" -exec rm -vf {} \;
-find /var/www/vhosts/plaatjesgenerator.nl/tegelizr/ -type f -name "*lala-lala*" -exec rm -vf {} \;
-find /var/www/vhosts/plaatjesgenerator.nl/tegelizr/ -type f -name "*langkous*" -exec rm -vf {} \;
-rm /var/www/vhosts/plaatjesgenerator.nl/tegelizr/deleted_files/thumbs/*;
-rm /var/www/vhosts/plaatjesgenerator.nl/tegelizr/deleted_files/tegeltjes/*;
-
+find /var/www/vhosts/plaatjesgenerator.nl/tegelizr.nl/ -type f -name "*van-harte-gefeliciteerd*" -exec rm -vf {} \;
+find /var/www/vhosts/plaatjesgenerator.nl/tegelizr.nl/ -type f -name "*hartelijk-gefeliciteerd*" -exec rm -vf {} \;
+find /var/www/vhosts/plaatjesgenerator.nl/tegelizr.nl/ -type f -name "*fijne-samenwerking*" -exec rm -vf {} \;
+find /var/www/vhosts/plaatjesgenerator.nl/tegelizr.nl/ -type f -name "*gefeliciteerd-met-je-verjaardag*" -exec rm -vf {} \;
+find /var/www/vhosts/plaatjesgenerator.nl/tegelizr.nl/ -type f -name "*woordvoerder-oeleh*" -exec rm -vf {} \;
+find /var/www/vhosts/plaatjesgenerator.nl/tegelizr.nl/ -type f -name "*woordvoerder-ouleh*" -exec rm -vf {} \;
+find /var/www/vhosts/plaatjesgenerator.nl/tegelizr.nl/ -type f -name "*gaat-met-pensioen*" -exec rm -vf {} \;
+find /var/www/vhosts/plaatjesgenerator.nl/tegelizr.nl/ -type f -name "*proficiat*" -exec rm -vf {} \;
+find /var/www/vhosts/plaatjesgenerator.nl/tegelizr.nl/ -type f -name "*lokwinske*" -exec rm -vf {} \;
+find /var/www/vhosts/plaatjesgenerator.nl/tegelizr.nl/ -type f -name "*langkous*" -exec rm -vf {} \;
+find /var/www/vhosts/plaatjesgenerator.nl/tegelizr.nl/ -type f -name "*youtube*" -exec rm -vf {} \;
+find /var/www/vhosts/plaatjesgenerator.nl/tegelizr.nl/ -type f -name "*2083*" -exec rm -vf {} \;
+find /var/www/vhosts/plaatjesgenerator.nl/tegelizr.nl/ -type f -name "*snicker*" -exec rm -vf {} \;
+find /var/www/vhosts/plaatjesgenerator.nl/tegelizr.nl/ -type f -name "*linkse-ratten*" -exec rm -vf {} \;
+find /var/www/vhosts/plaatjesgenerator.nl/tegelizr.nl/ -type f -name "*goud-olie-en-drugs*" -exec rm -vf {} \;
+find /var/www/vhosts/plaatjesgenerator.nl/tegelizr.nl/ -type f -name "*landverrader*" -exec rm -vf {} \;
+find /var/www/vhosts/plaatjesgenerator.nl/tegelizr.nl/ -type f -name "*appy-birthday*" -exec rm -vf {} \;
+find /var/www/vhosts/plaatjesgenerator.nl/tegelizr.nl/ -type f -name "*adolf-befbezem*" -exec rm -vf {} \;
+find /var/www/vhosts/plaatjesgenerator.nl/tegelizr.nl/ -type f -name "*amoorah*" -exec rm -vf {} \;
+find /var/www/vhosts/plaatjesgenerator.nl/tegelizr.nl/ -type f -name "*berber*" -exec rm -vf {} \;
+find /var/www/vhosts/plaatjesgenerator.nl/tegelizr.nl/ -type f -name "*paul-van-buuren*" -exec rm -vf {} \;
+find /var/www/vhosts/plaatjesgenerator.nl/tegelizr.nl/ -type f -name "*marokka*" -exec rm -vf {} \;
+find /var/www/vhosts/plaatjesgenerator.nl/tegelizr.nl/ -type f -name "*lavendelnazi*" -exec rm -vf {} \;
+find /var/www/vhosts/plaatjesgenerator.nl/tegelizr.nl/ -type f -name "*limburgers*" -exec rm -vf {} \;
+find /var/www/vhosts/plaatjesgenerator.nl/tegelizr.nl/ -type f -name "*zuigen*" -exec rm -vf {} \;
+find /var/www/vhosts/plaatjesgenerator.nl/tegelizr.nl/ -type f -name "*anaal-is-gewoon-astronomie*" -exec rm -vf {} \;
+find /var/www/vhosts/plaatjesgenerator.nl/tegelizr.nl/ -type f -name "*aivd-luistert-mee*" -exec rm -vf {} \;
+find /var/www/vhosts/plaatjesgenerator.nl/tegelizr.nl/ -type f -name "*frambozenjam*" -exec rm -vf {} \;
+find /var/www/vhosts/plaatjesgenerator.nl/tegelizr.nl/ -type f -name "*drek-drek*" -exec rm -vf {} \;
+find /var/www/vhosts/plaatjesgenerator.nl/tegelizr.nl/ -type f -name "*lala-lala*" -exec rm -vf {} \;
+find /var/www/vhosts/plaatjesgenerator.nl/tegelizr.nl/ -type f -name "*langkous*" -exec rm -vf {} \;
+rm /var/www/vhosts/plaatjesgenerator.nl/tegelizr.nl/deleted_files/thumbs/*;
+rm /var/www/vhosts/plaatjesgenerator.nl/tegelizr.nl/deleted_files/tegeltjes/*;
 
 echo "Opgeruimd staat netjes"
